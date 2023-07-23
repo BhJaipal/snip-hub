e6eeca7db45a125959478c7fad043105d45c4413
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-07-23 16:52:07 +0530

    Update routes/about.js

--- routes/about.js
+++ routes/about.js
@@ -3,5 +3,5 @@ let path= require("path");
 
 let router = express.Router();
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../"))
+    res.sendFile(path.join(__dirname, "../publi"))
 })
