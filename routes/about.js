a4b98f644c908ebc79187a3671d874c1a900dbd7
BhJaipal <jaipalbhanwariya001@gmail.com>
2023-08-20 07:53:31 +0530

    removing useless files

--- routes/about.js
+++
@@ -1,8 +0,0 @@
-let express = require("express");
-let path= require("path");
-
-let router = express.Router();
-router.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/about.html"));
-});
-exports.module = router;
--- routes/home.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let path= require("path");
-
-let router= express.Router();
-router("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
-exports.module = router;
