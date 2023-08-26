dd3150b2d6e988ac0d8e6156d9e8b1e2abf87d50
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-08-26 12:28:13 +0530

    tailwind and postcss config 1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/about": "http://localhost:9960"
-    }
-  }
+  plugins: [vue()]
 })
 
