993528660d8266ad4ea3f00f36c13a0f905afbb5
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-08-28 09:24:48 +0530

    Update frontend/src/components/About.vue

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -62,7 +62,7 @@ let navigation= ref<
       <div class="bg-transparent font-bold h-full pt-5 flex py-2 float-left text-2xl pl-2.5">About Page
       </div>
       <div class="flex justify-end py-2">
-        <button v-for="item in navigation" 
+        <button v-for="item in navigation"
           :key="item.name"
           class="text-white font-bold rounded-xl border-2 w-[60px] h-[55px] mt-[5px] bg-slate-900 mr-5 flex-row flex-nowrap hover:bg-indigo-950 cursor-pointor decoration-transparent">
           <router-link class="underline-none" v-bind:active="item.active" v-bind:to="item.href">
