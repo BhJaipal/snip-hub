dacf6a4e58b60a2c48d6979c7b55a67081d3b812
BhJaipal <jaipalbhanwariya001@gmail.com>
2023-09-02 14:47:28 +0530

    snippet page update

--- frontend/src/components/Home/snippet_page.vue
+++ frontend/src/components/Home/snippet_page.vue
@@ -1,24 +1,38 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import hljs from "highlight.js";
-const codeList= ref(null);
+import "./vs-dark.css"
+import highlighter from "./highlighter.vue";
+
+const codeList= ref([]);
 
 onMounted(() => {
-    fetch("http://localhost:3300")
+    fetch("http://localhost:3300/languages")
         .then(res => {return res.json();})
-        .then(data= codeList.value= data);
+        .then(data=> codeList.value= data);
     setTimeout(() => {
         hljs.highlightAll();
     }, 1000);
-}):
+});
 </script>
 <template>
-
+    <div v-if="!(codeList.length)">
+        <div id="loading"></div>
+    </div>
+    <div v-else>
+        <div v-for="langBox in codeList.value">
+            <h3 class="text-center">{{ langBox.langName.charAt(0).toUpperCase()+ langBox.langName.slice(1) }}</h3>
+            <highlighter v-for="codeBox in langBox['codeBoxes']"
+                v-bind:lang-name="langBox.langName"
+                v-bind:snip-title="codeBox.title"
+                v-bind:snip-code="codeBox.code"/>
+        </div>
+    </div>
 </template>
 <style scoped>
 #loading{
-    margin-top: 330px;
-    margin-left: 145px;
+    margin-top: 300px;
+    margin-left: 160px;
     width: 48px;
     height: 48px;
     border: 5px solid #3498db;
