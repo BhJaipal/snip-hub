d905a18965f81f18a6e22c8e2293d5580ad6bcf0
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-09-02 13:09:04 +0530

    Update frontend/src/components/Home/snippet_page.vue

--- frontend/src/components/Home/snippet_page.vue
+++ frontend/src/components/Home/snippet_page.vue
@@ -27,7 +27,7 @@ onMounted(() => {
                                19 ┊ animation: rotate 1s linear infinite
                                   20 }
                                      21
-                                        22 @keyframes rotate {
+                                         @keyframes rotate {
                                            0% {transform: rotate(0deg);}
                                                100% {transform: rotate(360deg);}
                                                   }
