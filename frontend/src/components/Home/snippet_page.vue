2bc6d534c3faa9120bc6b74ea0a5369bfde28520
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-09-02 13:11:25 +0530

    Update frontend/src/components/Home/snippet_page.vue

--- frontend/src/components/Home/snippet_page.vue
+++ frontend/src/components/Home/snippet_page.vue
@@ -27,7 +27,7 @@ onMounted(() => {
 }
 
 @keyframes rotate {
-                                           0% {transform: rotate(0deg);}
+    0% {transform: rotate(0deg);}
                                                100% {transform: rotate(360deg);}
     }
 }
