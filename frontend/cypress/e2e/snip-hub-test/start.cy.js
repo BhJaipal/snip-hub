298ef1f0bb6bacbc7c7f0cc190c18e73e93e067a
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-02-24 17:56:10 +0530

    Home Page and search page testing done

--- frontend/cypress/e2e/snip-hub-test/start.cy.js
+++ frontend/cypress/e2e/snip-hub-test/start.cy.js
@@ -11,7 +11,7 @@ describe('Start', () => {
     it("Input should work", () => {
         cy.wait(100);
         cy.get("input#search").type("hello").should("have.value", "hello");
-        cy.get("button#search-btn").click().location("href").should("contain", "http://localhost:3000/search/hello");
+        cy.get("button#search-btn").click().location("href").should("eq", "http://localhost:3000/search/hello");
     })
     it("Go to search page and come back", () => {
         cy.get("input#search").type("hello{enter}").location("href").should("contain", "http://localhost:3000/search/hello");
